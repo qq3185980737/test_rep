c0f436972ad710cedc45fb2b494f957fd68b096c
qq3185980737 <130239601+qq3185980737@users.noreply.github.com>
2025-06-30 01:09:06 +0800

    feature(law):添加乘法选项

--- sum_program.cpp
+++ sum_program.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 
 int main() {
+	// 声明变量
 	logical flag;
+	double num1, num2, sum;
 	// 提示用户输入第二个数字
 	std::cout << "请输入选项:0（求和），1（求积） ";
 	std::cin >> flag;
+
+
 	if (flag == 0)
 	{
-		// 声明变量
-		double num1, num2, sum;
 
 		// 提示用户输入第一个数字
 		std::cout << "请输入第一个数字: ";
@@ -26,7 +28,22 @@ int main() {
 
 	}
 	else {
-		std::cout << "等待完成 ";
+
+
+		// 提示用户输入第一个数字
+		std::cout << "请输入第一个数字: ";
+		std::cin >> num1;
+
+		// 提示用户输入第二个数字
+		std::cout << "请输入第二个数字: ";
+		std::cin >> num2;
+
+		// 计算两数之和
+		sum = num1 * num2;
+
+		// 显示结果
+		std::cout << num1 << " * " << num2 << " = " << sum << std::endl;
+
 	}
 	return 0;
 }
